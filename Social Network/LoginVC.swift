353fbd05de207ced524bb8ec528850e9792f3e93
supensa <stitch@Mobap.local>
2017-02-13 23:02:41 +1100

    First commit

---
+++ Social Network/LoginVC.swift
@@ -0,0 +1,25 @@
+//
+//  LoginVC.swift
+//  Social Network
+//
+//  Created by Spencer Forrest on 13/02/2017.
+//  Copyright © 2017 Spencer Forrest. All rights reserved.
+//
+
+import UIKit
+
+class LoginVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ readme.md
@@ -0,0 +1,10 @@
+# iOS10-SocialNetWork
+
+Creation of a simple Social App
+
+### Concepts Used:
+* Firebase:
+  * Authentication
+  * Database
+  * Storage
+
