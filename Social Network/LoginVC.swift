f10211e306449e0913505fb133228217ebd61fbc
supensa <stitch@Mobap.local>
2017-03-06 23:39:33 +1100

    LoginVC UIKeyboard update. (Still need to transfer code in respective views)

--- Social Network/LoginVC.swift
+++ Social Network/LoginVC.swift
@@ -12,13 +12,18 @@ import FacebookLogin
 import SwiftKeychainWrapper
 
 class LoginVC: UIViewController {
-
+    
     @IBOutlet weak var emailField: CustomTF!
     @IBOutlet weak var passwordField: CustomTF!
     
+    @IBOutlet weak var bodyViewBottomLC: NSLayoutConstraint!
+    
+    @IBOutlet weak var fbBtnHeightLC: NSLayoutConstraint!
+    @IBOutlet weak var orLbl: UILabel!
+    @IBOutlet weak var selectionLbl: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
     
     override func viewDidAppear(_ animated: Bool) {
@@ -28,11 +33,56 @@ class LoginVC: UIViewController {
     }
     
     override func viewWillAppear(_ animated: Bool) {
-        //
+        NotificationCenter.default.addObserver(self, selector: #selector(LoginVC.keyboardShowing(_:)), name: Notification.Name.UIKeyboardWillShow, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(LoginVC.keyboardHiding(_:)), name: Notification.Name.UIKeyboardWillHide, object: nil)
     }
     
     override func viewWillDisappear(_ animated: Bool) {
-        //
+        NotificationCenter.default.removeObserver(self, name: Notification.Name.UIKeyboardWillShow, object: nil)
+        NotificationCenter.default.removeObserver(self, name: Notification.Name.UIKeyboardWillHide, object: nil)
+    }
+    
+    func keyboardShowing(_ notification: NSNotification){
+        fbBtnHeightLC.constant = 0
+        selectionLbl.isHidden = true
+        orLbl.isHidden = true
+        
+        view.layoutIfNeeded()
+        
+        keyboardAppearance(notification)
+    }
+    
+    func keyboardHiding(_ notification: NSNotification){
+        fbBtnHeightLC.constant = 100
+        selectionLbl.isHidden = false
+        orLbl.isHidden = false
+        
+        view.layoutIfNeeded()
+        
+        keyboardAppearance(notification)
+    }
+    
+    // Break up this one in different functionned used differently if Keyboard Shows or Hide
+    private func keyboardAppearance(_ notification: NSNotification){
+        let userInfo = notification.userInfo!
+        let keyboardEndFrame = (userInfo[UIKeyboardFrameEndUserInfoKey] as! NSValue).cgRectValue
+        let convertedKeyboardEndFrame = view.convert(keyboardEndFrame, from: view.window)
+        
+        // Update size of bodyView layout
+        bodyViewBottomLC.constant = view.bounds.maxY - convertedKeyboardEndFrame.minY
+        
+        // Update HeaderView
+        //headerView.isHidden = !headerView.isHidden
+        
+        let rawAnimationCurve = (userInfo[UIKeyboardAnimationCurveUserInfoKey] as! NSNumber).intValue
+        let viewAnimationCurve = UIViewAnimationCurve(rawValue: rawAnimationCurve)!
+        let animationCurve = FormatCheckers.animationOptionsWithCurve(curve: viewAnimationCurve)
+        
+        let animationDuration = (userInfo[UIKeyboardAnimationDurationUserInfoKey] as! NSNumber).doubleValue
+        
+        UIView.animate(withDuration: animationDuration, delay: 0.0, options: [UIViewAnimationOptions.beginFromCurrentState,animationCurve], animations: { 
+            self.view.layoutIfNeeded()
+        }, completion: nil)
     }
     
     // Calls this function when the tap is recognized
